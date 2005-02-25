8282f1367075d9a4807ced100bc18176fe9b3427
Scott Deboy <sdeboy@apache.org>
2005-02-25 07:26:01 +0000

    Adding VFSLogFilePatternReceiver to list of known receivers Added 'clean' target to build.xml
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/chainsaw/trunk@308066 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/log4j/chainsaw/vfs/VFSLogFilePatternReceiverBeanInfo.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 1999,2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.log4j.chainsaw.vfs;
+
+import java.beans.PropertyDescriptor;
+import java.beans.SimpleBeanInfo;
+
+
+/**
+ * BeanInfo class for the meta-data of the VFSLogFilePatternReceiver.
+ *
+ */
+public class VFSLogFilePatternReceiverBeanInfo extends SimpleBeanInfo {
+  /* (non-Javadoc)
+   * @see java.beans.BeanInfo#getPropertyDescriptors()
+   */
+  public PropertyDescriptor[] getPropertyDescriptors() {
+    try {
+      return new PropertyDescriptor[] {
+        new PropertyDescriptor("fileURL", VFSLogFilePatternReceiver.class),
+        new PropertyDescriptor(
+          "timestampFormat", VFSLogFilePatternReceiver.class),
+        new PropertyDescriptor("logFormat", VFSLogFilePatternReceiver.class),
+        new PropertyDescriptor("name", VFSLogFilePatternReceiver.class),
+        new PropertyDescriptor("tailing", VFSLogFilePatternReceiver.class),
+        new PropertyDescriptor(
+          "filterExpression", VFSLogFilePatternReceiver.class),
+      };
+    } catch (Exception e) {
+    }
+
+    return null;
+  }
+}
