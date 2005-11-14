54016e0ec63824695ac2d730335a3bfb7cc25bed
Paul Jeffrey Smith <psmith@apache.org>
2005-11-14 22:04:12 +0000

    While I remember, updating release notes and triggering version change.
    git-svn-id: https://svn.apache.org/repos/asf/logging/chainsaw/trunk@344248 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/chainsaw/version/VersionManager.java
+++ src/java/org/apache/log4j/chainsaw/version/VersionManager.java
@@ -10,7 +10,7 @@ public class VersionManager
 
     private static final VersionManager instance = new VersionManager();
     
-    private static final String VERSION_INFO = "1.99.99 (22 February 2005)";
+    private static final String VERSION_INFO = "1.99.99 (15 November 2005)";
     
     public static final VersionManager getInstance() {
         return instance;
