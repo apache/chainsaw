441dba1cae74bf1ec64a27f323afad1abd17c031
Paul Jeffrey Smith <psmith@apache.org>
2005-11-18 05:55:53 +0000

    First cut of a test case that tests serializing LogPanelPreferenceModel to XML, nothing else changed.
    I'll be making modifications to add Column order/width and other 
    LogPanel preference related things a bit later, but I just wanted to make
    sure the current object could be serialized and restored ok.
    
    git-svn-id: https://svn.apache.org/repos/asf/logging/chainsaw/trunk@345455 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/org/apache/log4j/chainsaw/LogPanelPreferenceModelTest.java
@@ -0,0 +1,45 @@
+package org.apache.log4j.chainsaw;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+import junit.framework.TestCase;
+
+public class LogPanelPreferenceModelTest extends TestCase {
+
+    public void testLogPanelPreferenceModelSerialization() throws Exception {
+        LogPanelPreferenceModel model = new LogPanelPreferenceModel();
+        
+        
+        /**
+         * First modify the model from it's default state so we know we're actually storing
+         * something 'different' for deserialization tests
+         */
+        
+        model.setLevelIcons(!model.isLevelIcons());
+        model.setColumnVisible(ChainsawColumns.getColumnName(ChainsawColumns.INDEX_LOGGER_COL_NAME), false);
+        model.setDateFormatPattern("yyyyDDmm");
+        model.setLoggerPrecision("FATAL");
+        model.setLogTreePanelVisible(!model.isLogTreePanelVisible());
+        model.setScrollToBottom(model.isScrollToBottom());
+        model.setToolTips(!model.isToolTips());
+        
+        XStream stream = new XStream(new DomDriver());
+        String xml = stream.toXML(model);
+//        System.out.println(xml);
+        
+        LogPanelPreferenceModel restored = (LogPanelPreferenceModel) stream.fromXML(xml);
+        
+        assertEquals(model.isLevelIcons(), restored.isLevelIcons());
+        assertEquals(model.getDateFormatPattern(), restored.getDateFormatPattern());
+        assertEquals(model.getLoggerPrecision(), restored.getLoggerPrecision());
+        assertEquals(model.isLogTreePanelVisible(), restored.isLogTreePanelVisible());
+        assertEquals(model.isScrollToBottom(), restored.isScrollToBottom());
+        assertEquals(model.isToolTips(), restored.isToolTips());
+        
+        assertEquals(!model.isColumnVisible(ChainsawColumns.getColumnName(ChainsawColumns.INDEX_LOGGER_COL_NAME)), restored.isColumnVisible(ChainsawColumns.getColumnName(ChainsawColumns.INDEX_LOGGER_COL_NAME)));
+        
+        
+        
+    }
+}
