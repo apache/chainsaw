f866b8eb7ac5db84c5071d61bd73f6f4987545bd
Paul Jeffrey Smith <psmith@apache.org>
2005-11-21 02:31:35 +0000

    Added native OS X support to Chainsaw.
    Used dynamic(reflection) to determine if the current running OS is OSX
    and if the appropriate Mac Java extensions are available, and dynamically
    install the hooks so that the Menu bar appears where it should in OSX, plus
    place Preferences/About and Quit menu items where they should too (don't
    display them in the other menu items either).
    
    Using dynamic means removes the need for a user to have OS X to compile Chainsaw.
    
    Also added a "iSaw" target (yes it sounds like "eyesore"... can't think of anything else
    as yet) that uses jarbundler to create a ".app" folder of the Chainsaw distribution.
    
    I'm currently manually creating a .dmg file with Disk Utility, but hope to ant
    that step as well.
    
    Will get the user community to test the .dmg distribution and get feedback before placing
    it as a supported distribution from the website.
    
    git-svn-id: https://svn.apache.org/repos/asf/logging/chainsaw/trunk@345811 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/log4j/chainsaw/osx/OSXIntegration.java
@@ -0,0 +1,78 @@
+package org.apache.log4j.chainsaw.osx;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.apache.commons.vfs.tasks.ShowFileTask;
+import org.apache.log4j.Logger;
+import org.apache.log4j.chainsaw.LogUI;
+
+
+/**
+ * This class adds dynamic hooks into OSX version of Java so that various 
+ * Mac-specific UI guidelines are adhered to.
+ * 
+ * This class uses reflection to build the necessary hooks so that there is no compile-time
+ * dependency on a Mac SDK.
+ * 
+ * {@link  http://developer.apple.com/documentation/Java/index.html}
+ * @author psmith
+ *
+ */
+public class OSXIntegration {
+    public static final boolean IS_OSX = System.getProperty("os.name").startsWith("Mac OS X");
+    private static final Logger LOG = Logger.getLogger(OSXIntegration.class);
+    private static Object applicationInstance;
+    public static final void init(final LogUI logui) {
+        LOG.info("OSXIntegration.init() called");
+        if(!IS_OSX) {
+            LOG.info("Not OSX, ignoring...");
+            return;
+        }
+        try {
+            Class applicationClazz = Class.forName("com.apple.eawt.Application");
+            Class listenerClass = Class.forName("com.apple.eawt.ApplicationListener");
+            applicationInstance = applicationClazz.newInstance();
+            
+//            now register that we want that Preferences menu
+            Method enablePreferenceMethod = applicationClazz.getMethod("setEnabledPreferencesMenu", new Class[] {boolean.class});
+            enablePreferenceMethod.invoke(applicationInstance, new Object[] {Boolean.TRUE});
+            
+            
+            // no About menu for us for now.
+            Method enableAboutMethod = applicationClazz.getMethod("setEnabledAboutMenu", new Class[] {boolean.class});
+            enableAboutMethod.invoke(applicationInstance, new Object[] {Boolean.TRUE});
+            
+            // Need to create a Proxy object to represent an anonymous impl of the ApplicationListener class
+            Object listenerProxy = Proxy.newProxyInstance(OSXIntegration.class.getClassLoader(), 
+                        new Class[] {listenerClass}, 
+                        new InvocationHandler() {
+
+                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                    if("handlePreferences".equals(method.getName())){
+                        LOG.info("handlePreferences(...) called");
+                        logui.showApplicationPreferences();
+                    }else if("handleQuit".equals(method.getName())){
+                        setHandled(args[0], logui.shutdown()?Boolean.TRUE:Boolean.FALSE);
+                    }else if("handleAbout".equals(method.getName())) {
+                        logui.showAboutBox();
+                        setHandled(args[0], Boolean.TRUE);
+                    }
+//                    TODO think about File Open/Save options
+                    return null;
+                }
+
+                private void setHandled(Object event, Boolean val) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+                    Method handleMethod =   event.getClass().getMethod("setHandled", new Class[] {boolean.class});
+                    handleMethod.invoke(event, new Object[] {val});
+                }});           
+            // register the proxy object via the addApplicationListener method, cross fingers...
+            Method registerListenerMethod = applicationClazz.getMethod("addApplicationListener", new Class[] {listenerClass});
+            registerListenerMethod.invoke(applicationInstance, new Object[] {listenerProxy});
+        } catch (Exception e) {
+            LOG.error("Failed to setup OSXIntegration", e);
+        }
+    }
+}
