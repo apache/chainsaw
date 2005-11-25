c68b377770512b6535287837b0dff785904471eb
Paul Jeffrey Smith <psmith@apache.org>
2005-11-25 07:03:19 +0000

    This change makes this class binary compatible with log4j 1.2.x.
    Some would call this hacky.  I would too.
    
    git-svn-id: https://svn.apache.org/repos/asf/logging/chainsaw/trunk@348903 13f79535-47bb-0310-9956-ffa450edef68

--- zeroconf/org/apache/log4j/net/ZeroConfSocketHubAppender.java
+++ zeroconf/org/apache/log4j/net/ZeroConfSocketHubAppender.java
@@ -1,9 +1,12 @@
 package org.apache.log4j.net;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceInfo;
+
+import org.apache.log4j.Level;
 
 
 /**
@@ -22,18 +25,51 @@ public class ZeroConfSocketHubAppender extends SocketHubAppender {
     
     private String zeroConfDeviceName = "SocketHubAppender";
     
+    private Object logger;
+    private Method logInfoMethod;
+    private Method logErrorMethod;
+    
+    public ZeroConfSocketHubAppender() {
+        try {
+            Method getLoggerMethod = this.getClass().getMethod("getLogger", new Class[0]);
+            logger = getLoggerMethod.invoke(this, new Object[0]);
+            logInfoMethod = logger.getClass().getMethod("info", new Class[] {Object.class});
+            logErrorMethod = logger.getClass().getMethod("error", new Class[] {Object.class});
+        }catch(Exception e) {
+            // we're not in log4j1.3 land
+        }
+    }
     public void activateOptions() {
         super.activateOptions();
         
         try {
             JmDNS jmDNS = Zeroconf4log4j.getInstance();
-            ServiceInfo info = new ServiceInfo(zeroConfZone, zeroConfDeviceName, getPort(), "SocketHubAppender on port " + getPort() );
-            getLogger().info("Registering this SocketHubAppender as :" + info);
+            ServiceInfo info = buildServiceInfo();
+            logWithlog4j12Compatibility(Level.INFO,"Registering this SocketHubAppender as :" + info);
             jmDNS.registerService(info);
         } catch (IOException e) {
-            getLogger().error("Failed to instantiate JmDNS to broadcast via ZeroConf, will now operate in simple SocketHubAppender mode");
+            logWithlog4j12Compatibility(Level.ERROR,"Failed to instantiate JmDNS to broadcast via ZeroConf, will now operate in simple SocketHubAppender mode");
         }
-        
+    }
+    private ServiceInfo buildServiceInfo() {
+        return new ServiceInfo(zeroConfZone, zeroConfDeviceName, getPort(), "SocketHubAppender on port " + getPort() );
+    }
+    
+    private void logWithlog4j12Compatibility(Level level, String message) {
+        if(logger!=null && logInfoMethod!=null & logErrorMethod!=null) {
+            try {
+                switch (level.toInt()) {
+                case Level.INFO_INT:
+                    logInfoMethod.invoke(logger, new Object[] { message });
+                    break;
+                case Level.ERROR_INT:
+                    logInfoMethod.invoke(logger, new Object[] { message });
+                    break;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     /**
@@ -84,4 +120,17 @@ public class ZeroConfSocketHubAppender extends SocketHubAppender {
 //        TODO work out a sane checking mechanism that verifies the value is a correct ZeroConf zone
         this.zeroConfZone = zeroConfZone;
     }
+    public synchronized void close() {
+        super.close();
+        try {
+            JmDNS jmDNS = Zeroconf4log4j.getInstance();
+            ServiceInfo info = buildServiceInfo();
+            logWithlog4j12Compatibility(Level.INFO,"Deregistering this SocketHubAppender (" + info + ")");
+            jmDNS.unregisterService(info);
+        } catch (Exception e) {
+            logWithlog4j12Compatibility(Level.ERROR,"Failed to instantiate JmDNS to broadcast via ZeroConf, will now operate in simple SocketHubAppender mode");
+        }
+    }
+    
+    
 }
